6ff929db973fc16925ebd27693c67e20e2c70046
kazuya kawaguchi <kawakazu80@gmail.com>
2021-09-12 02:22:12 +0900

    commit first

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = {
+  root: true,
+  globals: {},
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  rules: {}
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# @intlify/nuxt3
+
+Nuxt 3 module for vue-i18n-next
+
+> ⚠️ NOTE: This is a WIP module for Nuxt 3, It isn't available yet.
+
+# 🌟 Features
+- Setup `vue-i18n` for your Nuxt project
+
+# 🚀 Usage
+Add `@intlify/nuxt3` module to `buildModules` options of `nuxt.confg.[ts|js]`
+
+```js
+export default {
+  buildModules: [
+    ['@intlify/nuxt3']
+  ]
+}
+```
+
+# ©️ LICENSE
+MIT
